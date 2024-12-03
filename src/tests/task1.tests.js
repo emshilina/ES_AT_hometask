481b6f3633518b29113523f69e64cfe090c0c912
Elena Shilina <elena_shilina@epam.com>
2024-12-03 20:32:17 +0100

    Task_1_Test_4

--- src/tests/task1.tests.js
+++ src/tests/task1.tests.js
@@ -1,12 +1,16 @@
 describe('Task 1', () => {
-    it('Check the title is correct', async () => {
+    beforeEach(async () => {
         await browser.url('https://www.epam.com/');
+    });
+   
+    it('Check the title is correct', async () => {
+        
         await expect(browser).toHaveTitle('EPAM | Software Engineering & Product Development Services')
 
     });
 
     it('Check the ability to switch Light / Dark mode', async () => {
-        await browser.url('https://www.epam.com/');
+        //await browser.url('https://www.epam.com/');
 
         await $('.header__vaulting-container .theme-switcher').click();
 
@@ -17,11 +21,22 @@ describe('Task 1', () => {
     });
 
     it('Check that allow to change language to UA', async () => {
-        await browser.url('https://www.epam.com/');
+        //await browser.url('https://www.epam.com/');
         await $('.location-selector__button').click();
         await $(':nth-child(6) > .location-selector__link').click();
 
         await expect(browser).toHaveTitle('EPAM Ukraine - найбільша ІТ-компанія в Україні | Вакансії')
 
+     });
+
+     
+    it('Check the policies list', async () => {
+        await expect($('.policies > .heading')).toHaveText('POLICIES');
+        await expect($('.policies-left > :nth-child(1) > .fat-links')).toHaveText('INVESTORS');
+        await expect($('.policies-left > :nth-child(5) > .fat-links')).toHaveText('COOKIE POLICY');
+        await expect($('.policies-left > :nth-child(2) > .fat-links')).toHaveText('OPEN SOURCE');
+        await expect($('.policies-right > :nth-child(1) > .fat-links')).toHaveText('APPLICANT PRIVACY NOTICE');
+        await expect($('.policies-left > :nth-child(3) > .fat-links')).toHaveText('PRIVACY POLICY');
+        await expect($('.policies-right > :nth-child(2) > .fat-links')).toHaveText('WEB ACCESSIBILITY');
      });
 })
